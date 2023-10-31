aa10da6a057fd7716914580f6c8fd43a37f19319
Mark <mark.de.lima@outlook.com>
2023-10-31 12:53:07 -0400

    Added comments and looked at logical flow of code

--- Encryption.c
+++ Encryption.c
@@ -2,5 +2,19 @@
 
 int main()
 {
-    printf("This works!!!");
+    /**
+     * ***** Has two command line switches *****
+     *
+     * ***** takes a file (type .txt) and reads the text *****
+     *
+     * ***** Uses text file and encrypts the characters by converting *****
+     * ***** them to an integer (ASCII), subtracting 16, then converting to hex *****
+     *
+     * ***** Change file type to .crp. Base file stays the same *****
+     *
+     * ***** Changes file type to .txt for decryption. Only one can exist *****
+     *
+     *  will need a loop. also will need to canibalize the code from Lab 5pt3
+     *
+     */
 }
