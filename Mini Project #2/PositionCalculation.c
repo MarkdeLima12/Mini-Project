03030cc4b2bc0964817a9e000d6f7edd0bf8b277
Mark <mark.de.lima@outlook.com>
2023-11-10 21:16:39 -0500

    Started working on taking in values and function

--- Mini Project #2/PositionCalculation.c
+++ Mini Project #2/PositionCalculation.c
@@ -6,4 +6,20 @@ double distance(double lat1, double lat2, double long1, double long2, double alt
 
 int main()
 {
+    int numPeople;
+    float latitude[], longitude[], altitude[];
+    printf("How many people are there: ");
+    scanf("%d", &numPeople);
+
+    for (int i = 1; i <= numPeople; i++)
+    {
+        printf("Please enter the latitude of Person #%f: ", i);
+        scanf("%f", &latitude[i]);
+
+        printf("Please enter the longitude of Person #%f: ", i);
+        scanf("%f", &longitude[i]);
+
+        printf("Please enter the altitude of Person #%f: ", i);
+        scanf("%f", &altitude[i]);
+    }
 }
